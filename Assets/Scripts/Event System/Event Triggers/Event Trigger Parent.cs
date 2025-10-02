c3abc90d852db0713d8c8aa41738e96dcc48e575
REM-Iros <remiros910@gmail.com>
2025-10-02 17:01:46 -0600

    Added basic event skeleton, need to add triggers and events.

---
+++ Assets/Scripts/Event System/Event Triggers/Event Trigger Parent.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// This abstract class represents the parent script for the different event triggers that
+/// will be connected to the Event Controller.  
+/// 
+/// REM-i
+/// </summary>
+[RequireComponent (typeof(EventController))]
+public abstract class EventTriggerParent : MonoBehaviour
+{
+    
+}
